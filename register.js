e8befb056ce5b28b4d7586fc3315827c4c3129a3
Donald Atkinson <fuzzicallogic@outlook.com>
2016-01-02 02:11:45 -0700

    Initial Element Setup

---
+++ register.js
@@ -0,0 +1,32 @@
+(function registerElement(ROOT_ELEMENT) {
+
+	Polymer({
+		is: 'native-title-bar',
+
+		created: onElementCreated,
+		attached: onElementAttached,
+		ready: onElementReady,
+		detached: onElementDetached,
+
+		properties: {
+		},
+
+	});
+
+	function onElementCreated() {
+
+	}
+
+	function onElementAttached() {
+
+	}
+
+	function onElementReady() {
+
+	}
+
+	function onElementDetached() {
+
+	}
+
+}) (document.querySelector('html'));
